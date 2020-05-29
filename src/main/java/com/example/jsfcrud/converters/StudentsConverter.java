0f5abdf8f0ebdfe3cc66ed40c199fdecd8d31df6
Lázaro Nixon <lazaronixon@hotmail.com>
2020-05-29 20:25:18 -0300

    converter example

---
+++ src/main/java/com/example/jsfcrud/converters/StudentsConverter.java
@@ -0,0 +1,27 @@
+package com.example.jsfcrud.converters;
+
+import com.example.jsfcrud.models.Student;
+import com.example.jsfcrud.services.StudentsService;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.inject.Inject;
+
+@FacesConverter(value = "studentsConverter", managed = true)
+public class StudentsConverter implements Converter {
+
+    @Inject
+    private StudentsService studentsService;
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        return value != null ? studentsService.find(Integer.parseInt(value)) : null;
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        return value != null ? String.valueOf(((Student) value).getId()) : null;
+    }
+
+}
