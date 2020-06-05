f32cbf3652c5db0ff6844820f80855f2809e42f7
Lázaro Nixon <lazaronixon@hotmail.com>
2020-06-05 02:22:19 -0300

    introduce timestamp

--- src/main/java/com/example/jsfcrud/models/ApplicationRecord.java
+++ src/main/java/com/example/jsfcrud/models/ApplicationRecord.java
@@ -1,14 +1,15 @@
 package com.example.jsfcrud.models;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 
 @MappedSuperclass
 public abstract class ApplicationRecord<ID> {
 
     public abstract ID getId();
-
-    public abstract void setId(ID id);
 
     public boolean isNewRecord() {
         return getId() == null;
@@ -16,6 +17,14 @@ public abstract class ApplicationRecord<ID> {
 
     public boolean isPersisted() {
         return getId() != null;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        // If you want timestamp it should be overridden on child.
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        // If you want timestamp it should be overridden on child.
     }
 
     @Override
@@ -28,6 +37,17 @@ public abstract class ApplicationRecord<ID> {
         return (getId() != null && getClass().isInstance(other) && other.getClass().isInstance(this))
                 ? getId().equals(((ApplicationRecord<?>) other).getId())
                 : (other == this);
+    }
+
+    @PrePersist
+    private void prePersist() {
+        setCreatedAt(LocalDateTime.now());
+        setUpdatedAt(LocalDateTime.now());
+    }
+
+    @PreUpdate
+    private void preUpdate() {
+        setUpdatedAt(LocalDateTime.now());
     }
 
 }
