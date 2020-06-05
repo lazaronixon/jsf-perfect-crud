36265b97dfc1d7ac293022408b7d48c5b6b3c92b
Lázaro Nixon <lazaronixon@hotmail.com>
2020-06-05 10:01:32 -0300

    more query methods

--- src/main/java/com/example/jsfcrud/services/ApplicationService.java
+++ src/main/java/com/example/jsfcrud/services/ApplicationService.java
@@ -3,9 +3,18 @@ package com.example.jsfcrud.services;
 import com.example.jsfcrud.models.ApplicationRecord;
 import static java.lang.String.format;
 import java.util.List;
+import static java.util.stream.IntStream.range;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 public abstract class ApplicationService<T> {
+
+    private final static String SELECT_STAR = "SELECT this FROM %s this";
+    private final static String SELECT_STAR_ORDER = "SELECT this FROM %s this ORDER BY %s";
+    private final static String SELECT_STAR_WHERE = "SELECT this FROM %s this WHERE %s";
+    private final static String SELECT_STAR_WHERE_ORDER = "SELECT this FROM %s this WHERE %s ORDER BY %s";
+    private final static String SELECT_COUNT_STAR = "SELECT COUNT(this) FROM %s this";
+    private final static String SELECT_COUNT_STAR_WHERE = "SELECT COUNT(this) FROM %s this WHERE %s";
 
     private final Class<T> entityClass;
 
@@ -14,7 +23,7 @@ public abstract class ApplicationService<T> {
     }
 
     public abstract EntityManager getEntityManager();
-    
+
     public abstract T find(String id);
 
     public void create(ApplicationRecord entity) {
@@ -42,19 +51,55 @@ public abstract class ApplicationService<T> {
     }
 
     public List<T> all() {
-        return getEntityManager().createQuery(format("SELECT this FROM %s this", entityClass.getName())).getResultList();
+        return getEntityManager().createQuery(format(SELECT_STAR, entityClass.getName())).getResultList();
     }
-    
+
     public List<T> all(String order) {
-        return getEntityManager().createQuery(format("SELECT this FROM %s this ORDER BY %s", entityClass.getName(), order)).getResultList();
+        return getEntityManager().createQuery(format(SELECT_STAR_ORDER, entityClass.getName(), order)).getResultList();
     }
-    
-    public List<T> allInRange(int first, int pageSize) {
-        return getEntityManager().createQuery(format("SELECT this FROM %s this", entityClass.getSimpleName())).setFirstResult(first).setMaxResults(pageSize).getResultList();
+
+    public List<T> all(int limit, int offset) {
+        return getEntityManager().createQuery(format(SELECT_STAR, entityClass.getSimpleName())).setMaxResults(limit).setFirstResult(offset).getResultList();
     }
-    
+
+    public List<T> all(int limit, int offset, String order) {
+        return getEntityManager().createQuery(format(SELECT_STAR_ORDER, entityClass.getSimpleName(), order)).setMaxResults(limit).setFirstResult(offset).getResultList();
+    }
+
+    public List<T> where(String conditions, Object... params) {
+        Query query = getEntityManager().createQuery(format(SELECT_STAR_WHERE, entityClass.getName(), conditions));
+        setPositionalParameters(query, params);
+        return query.getResultList();
+    }
+
+    public List<T> where(String conditions, String order, Object... params) {
+        Query query = getEntityManager().createQuery(format(SELECT_STAR_WHERE_ORDER, entityClass.getName(), conditions, order));
+        setPositionalParameters(query, params);
+        return query.getResultList();
+    }
+
+    public List<T> where(String conditions, int limit, int offset, Object... params) {
+        Query query = getEntityManager().createQuery(format(SELECT_STAR_WHERE, entityClass.getName(), conditions)).setMaxResults(limit).setFirstResult(offset);
+        setPositionalParameters(query, params);
+        return query.getResultList();
+    }
+
+    public List<T> where(String conditions, int limit, int offset, String order, Object... params) {
+        Query query = getEntityManager().createQuery(format(SELECT_STAR_WHERE_ORDER, entityClass.getName(), conditions, order)).setMaxResults(limit).setFirstResult(offset);
+        setPositionalParameters(query, params);
+        return query.getResultList();
+    }
+
     public Long count() {
-        return getEntityManager().createQuery(format("SELECT COUNT(this) FROM %s this", entityClass.getSimpleName()), Long.class).getSingleResult();
-    }    
+        return getEntityManager().createQuery(format(SELECT_COUNT_STAR, entityClass.getSimpleName()), Long.class).getSingleResult();
+    }
+
+    public Long count(String conditions) {
+        return getEntityManager().createQuery(format(SELECT_COUNT_STAR_WHERE, entityClass.getSimpleName(), conditions), Long.class).getSingleResult();
+    }
+
+    private void setPositionalParameters(Query query, Object[] positionalParameters) {
+        range(0, positionalParameters.length).forEach(i -> query.setParameter(i, positionalParameters[i]));
+    }
 
 }
