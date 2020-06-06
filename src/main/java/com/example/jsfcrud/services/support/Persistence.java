086d8933c3ebe9f539213a1f8c7ae3e614bba5bf
Lázaro Nixon <lazaronixon@hotmail.com>
2020-06-06 18:32:09 -0300

    flush as soon as possible

--- src/main/java/com/example/jsfcrud/services/support/Persistence.java
+++ src/main/java/com/example/jsfcrud/services/support/Persistence.java
@@ -6,21 +6,24 @@ public interface Persistence<T> extends EntityManager<T> {
 
     public default void create(ApplicationRecord entity) {
         getEntityManager().persist(entity);
+        getEntityManager().flush();
     }
 
     public default void update(ApplicationRecord entity) {
         getEntityManager().merge(entity);
+        getEntityManager().flush();
     }
 
     public default void destroy(ApplicationRecord entity) {
         getEntityManager().remove(getEntityManager().merge(entity));
+        getEntityManager().flush();
     }
 
     public default void save(ApplicationRecord entity) {
         if (entity.isNewRecord()) {
-            getEntityManager().persist(entity);
+            create(entity);
         } else {
-            getEntityManager().merge(entity);
+            update(entity);
         }
     }
 
