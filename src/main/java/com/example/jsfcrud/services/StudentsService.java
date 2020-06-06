12994c6cdd0bacd8deab49076169976af2a36743
Lázaro Nixon <lazaronixon@hotmail.com>
2020-06-06 20:17:28 -0300

    transactional on persistence

--- src/main/java/com/example/jsfcrud/services/StudentsService.java
+++ src/main/java/com/example/jsfcrud/services/StudentsService.java
@@ -5,9 +5,7 @@ import java.io.Serializable;
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 
-@Transactional
 @ApplicationScoped
 public class StudentsService extends ApplicationService<Student> implements Serializable {
 
--- src/main/java/com/example/jsfcrud/services/support/Persistence.java
+++ src/main/java/com/example/jsfcrud/services/support/Persistence.java
@@ -1,21 +1,26 @@
 package com.example.jsfcrud.services.support;
 
 import com.example.jsfcrud.models.ApplicationRecord;
+import javax.transaction.Transactional;
 
 public interface Persistence<T> extends EntityManager<T> {
 
+    @Transactional
     public default void create(ApplicationRecord entity) {
         getEntityManager().persist(entity);
     }
 
+    @Transactional
     public default void update(ApplicationRecord entity) {
         getEntityManager().merge(entity);
     }
 
+    @Transactional
     public default void destroy(ApplicationRecord entity) {
         getEntityManager().remove(getEntityManager().merge(entity));
     }
 
+    @Transactional
     public default void save(ApplicationRecord entity) {
         if (entity.isNewRecord()) {
             getEntityManager().persist(entity);
