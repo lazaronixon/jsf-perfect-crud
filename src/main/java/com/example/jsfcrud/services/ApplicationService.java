fd66743c5c94b1966b75b756adc261340470c066
Lázaro Nixon <lazaronixon@hotmail.com>
2020-06-05 22:01:55 -0300

    more work

--- src/main/java/com/example/jsfcrud/services/ApplicationService.java
+++ src/main/java/com/example/jsfcrud/services/ApplicationService.java
@@ -5,16 +5,25 @@ import static java.lang.String.format;
 import java.util.List;
 import static java.util.stream.IntStream.range;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 public abstract class ApplicationService<T> {
 
-    private final static String SELECT_ALL = "SELECT this FROM %s this";
-    private final static String SELECT_ALL_ORDER = "SELECT this FROM %s this ORDER BY %s";
-    private final static String SELECT_ALL_WHERE = "SELECT this FROM %s this WHERE %s";
-    private final static String SELECT_ALL_WHERE_ORDER = "SELECT this FROM %s this WHERE %s ORDER BY %s";
-    private final static String SELECT_COUNT_ALL = "SELECT COUNT(this) FROM %s this";
-    private final static String SELECT_COUNT_ALL_WHERE = "SELECT COUNT(this) FROM %s this WHERE %s";
+    private final static String SELECT_STAR = "SELECT this FROM %s this";
+    private final static String SELECT_STAR_ORDER = "SELECT this FROM %s this ORDER BY %s";
+    private final static String SELECT_STAR_WHERE = "SELECT this FROM %s this WHERE %s";
+    private final static String SELECT_STAR_WHERE_ORDER = "SELECT this FROM %s this WHERE %s ORDER BY %s";
+    private final static String SELECT_COUNT_STAR = "SELECT COUNT(this) FROM %s this";
+    private final static String SELECT_COUNT_STAR_WHERE = "SELECT COUNT(this) FROM %s this WHERE %s";
+    private final static String SELECT_AVERAGE = "SELECT AVG(%s) FROM %s this";
+    private final static String SELECT_AVERAGE_WHERE = "SELECT AVG(%s) FROM %s this WHERE %s";
+    private final static String SELECT_MINIMUM = "SELECT MIN(%s) FROM %s this";
+    private final static String SELECT_MINIMUM_WHERE = "SELECT MIN(%s) FROM %s this WHERE %s";
+    private final static String SELECT_MAXIMUM = "SELECT MAX(%s) FROM %s this";
+    private final static String SELECT_MAXIMUM_WHERE = "SELECT MAX(%s) FROM %s this WHERE %s";
+    private final static String SELECT_SUM = "SELECT SUM(%s) FROM %s this";
+    private final static String SELECT_SUM_WHERE = "SELECT SUM(%s) FROM %s this WHERE %s";
     private final static String SELECT_1_AS_ONE = "SELECT 1 AS one FROM %s this WHERE %s";
 
     private final Class<T> entityClass;
@@ -52,55 +61,87 @@ public abstract class ApplicationService<T> {
     }
 
     public T findBy(String condition, Object... params) {
-        return parametize(createQuery(format(SELECT_ALL, entityName())), params).setMaxResults(1).getResultStream().findFirst().orElse(null);
+        return parametize(createQuery(format(SELECT_STAR, entityName())), params).setMaxResults(1).getResultStream().findFirst().orElse(null);
     }
 
     public List<T> all() {
-        return createQuery(format(SELECT_ALL, entityName())).getResultList();
+        return createQuery(format(SELECT_STAR, entityName())).getResultList();
     }
 
     public List<T> order(String order) {
-        return createQuery(format(SELECT_ALL_ORDER, entityName(), order)).getResultList();
+        return createQuery(format(SELECT_STAR_ORDER, entityName(), order)).getResultList();
     }
 
-    public List<T> order(String order, int limit) {
-        return createQuery(format(SELECT_ALL_ORDER, entityName(), order)).setMaxResults(limit).getResultList();
+    public List<T> orderLimit(String order, int limit) {
+        return createQuery(format(SELECT_STAR_ORDER, entityName(), order)).setMaxResults(limit).getResultList();
     }
 
-    public List<T> order(String order, int limit, int offset) {
-        return createQuery(format(SELECT_ALL_ORDER, entityName(), order)).setMaxResults(limit).setFirstResult(offset).getResultList();
+    public List<T> orderInRange(String order, int limit, int offset) {
+        return createQuery(format(SELECT_STAR_ORDER, entityName(), order)).setMaxResults(limit).setFirstResult(offset).getResultList();
     }
 
     public List<T> where(String condition, Object... params) {
-        return parametize(createQuery(format(SELECT_ALL_WHERE, entityName(), condition)), params).getResultList();
+        return parametize(createQuery(format(SELECT_STAR_WHERE, entityName(), condition)), params).getResultList();
     }
 
     public List<T> whereLimit(String condition, int limit, Object... params) {
-        return parametize(createQuery(format(SELECT_ALL_WHERE, entityName(), condition)), params).setMaxResults(limit).getResultList();
+        return parametize(createQuery(format(SELECT_STAR_WHERE, entityName(), condition)), params).setMaxResults(limit).getResultList();
     }
 
     public List<T> whereInRange(String condition, int limit, int offset, Object... params) {
-        return parametize(createQuery(format(SELECT_ALL_WHERE, entityName(), condition)), params).setMaxResults(limit).setFirstResult(offset).getResultList();
+        return parametize(createQuery(format(SELECT_STAR_WHERE, entityName(), condition)), params).setMaxResults(limit).setFirstResult(offset).getResultList();
     }
 
     public List<T> whereOrder(String condition, String order, Object... params) {
-        return parametize(createQuery(format(SELECT_ALL_WHERE_ORDER, entityName(), condition, order)), params).getResultList();
+        return parametize(createQuery(format(SELECT_STAR_WHERE_ORDER, entityName(), condition, order)), params).getResultList();
     }
 
     public List<T> whereOrderLimit(String condition, String order, int limit, Object... params) {
-        return parametize(createQuery(format(SELECT_ALL_WHERE_ORDER, entityName(), condition, order)), params).setMaxResults(limit).getResultList();
+        return parametize(createQuery(format(SELECT_STAR_WHERE_ORDER, entityName(), condition, order)), params).setMaxResults(limit).getResultList();
     }
 
     public List<T> whereOrderInRange(String condition, String order, int limit, int offset, Object... params) {
-        return parametize(createQuery(format(SELECT_ALL_WHERE_ORDER, entityName(), condition, order)), params).setMaxResults(limit).setFirstResult(offset).getResultList();
+        return parametize(createQuery(format(SELECT_STAR_WHERE_ORDER, entityName(), condition, order)), params).setMaxResults(limit).setFirstResult(offset).getResultList();
     }
 
     public Long count() {
-        return createQuery(format(SELECT_COUNT_ALL, entityName()), Long.class).getSingleResult();
+        return createQuery(format(SELECT_COUNT_STAR, entityName()), Long.class).getSingleResult();
     }
 
-    public Long count(String condition, Object... params) {
-        return parametize(createQuery(format(SELECT_COUNT_ALL_WHERE, entityName(), condition), Long.class), params).getSingleResult();
+    public Long countWhere(String condition, Object... params) {
+        return parametize(createQuery(format(SELECT_COUNT_STAR_WHERE, entityName(), condition), Long.class), params).getSingleResult();
+    }
+
+    public <R> R average(String field, Class<R> resultClass) {
+        return createQuery(format(SELECT_AVERAGE, field, entityName()), resultClass).getSingleResult();
+    }
+
+    public <R> R averageWhere(String field, Class<R> resultClass, String condition, Object... params) {
+        return parametize(createQuery(format(SELECT_AVERAGE_WHERE, field, entityName(), condition), resultClass), params).getSingleResult();
+    }
+
+    public <R> R minimum(String field, Class<R> resultClass) {
+        return createQuery(format(SELECT_MINIMUM, field, entityName()), resultClass).getSingleResult();
+    }
+
+    public <R> R minimumWhere(String field, Class<R> resultClass, String condition, Object... params) {
+        return parametize(createQuery(format(SELECT_MINIMUM_WHERE, field, entityName(), condition), resultClass), params).getSingleResult();
+    }
+
+    public <R> R maximum(String field, Class<R> resultClass) {
+        return createQuery(format(SELECT_MAXIMUM, field, entityName()), resultClass).getSingleResult();
+    }
+
+    public <R> R maximumWhere(String field, Class<R> resultClass, String condition, Object... params) {
+        return parametize(createQuery(format(SELECT_MAXIMUM_WHERE, field, entityName(), condition), resultClass), params).getSingleResult();
+    }
+
+    public <R> R sum(String field, Class<R> resultClass) {
+        return createQuery(format(SELECT_SUM, field, entityName()), resultClass).getSingleResult();
+    }
+
+    public <R> R sumWhere(String field, Class<R> resultClass, String condition, Object... params) {
+        return parametize(createQuery(format(SELECT_SUM_WHERE, field, entityName(), condition), resultClass), params).getSingleResult();
     }
 
     public boolean exists(String condition, Object... params) {
@@ -111,13 +152,20 @@ public abstract class ApplicationService<T> {
         return getEntityManager().createQuery(qlString, entityClass);
     }
 
-    protected <T> TypedQuery<T> createQuery(String qlString, Class<T> resultClass) {
+    protected TypedQuery<T> createNamedQuery(String name) {
+        return getEntityManager().createNamedQuery(name, entityClass);
+    }
+
+    protected <R> TypedQuery<R> createQuery(String qlString, Class<R> resultClass) {
         return getEntityManager().createQuery(qlString, resultClass);
     }
 
-    private <T> TypedQuery<T> parametize(TypedQuery<T> query, Object[] parameters) {
-        range(0, parameters.length).forEach(i -> query.setParameter(i + 1, parameters[i]));
-        return query;
+    protected Query createNativeQuery(String sqlString, Class resultClass) {
+        return getEntityManager().createNativeQuery(sqlString, resultClass);
+    }
+
+    private <R> TypedQuery<R> parametize(TypedQuery<R> query, Object[] parameters) {
+        range(0, parameters.length).forEach(i -> query.setParameter(i + 1, parameters[i])); return query;
     }
 
     private String entityName() {
