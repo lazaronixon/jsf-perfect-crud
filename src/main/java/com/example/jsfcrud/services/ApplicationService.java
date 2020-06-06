a201c278a2047a40f9bf168c5c6e1d56eac3ddd5
Lázaro Nixon <lazaronixon@hotmail.com>
2020-06-05 21:28:21 -0300

    introduce query helpers

--- src/main/java/com/example/jsfcrud/services/ApplicationService.java
+++ src/main/java/com/example/jsfcrud/services/ApplicationService.java
@@ -5,17 +5,17 @@ import static java.lang.String.format;
 import java.util.List;
 import static java.util.stream.IntStream.range;
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 public abstract class ApplicationService<T> {
 
-    private final static String SELECT_STAR = "SELECT this FROM %s this";
-    private final static String SELECT_STAR_ORDER = "SELECT this FROM %s this ORDER BY %s";
-    private final static String SELECT_STAR_WHERE = "SELECT this FROM %s this WHERE %s";
-    private final static String SELECT_STAR_WHERE_ORDER = "SELECT this FROM %s this WHERE %s ORDER BY %s";
-    private final static String SELECT_COUNT_STAR = "SELECT COUNT(this) FROM %s this";
-    private final static String SELECT_COUNT_STAR_WHERE = "SELECT COUNT(this) FROM %s this WHERE %s";
+    private final static String SELECT_ALL = "SELECT this FROM %s this";
+    private final static String SELECT_ALL_ORDER = "SELECT this FROM %s this ORDER BY %s";
+    private final static String SELECT_ALL_WHERE = "SELECT this FROM %s this WHERE %s";
+    private final static String SELECT_ALL_WHERE_ORDER = "SELECT this FROM %s this WHERE %s ORDER BY %s";
+    private final static String SELECT_COUNT_ALL = "SELECT COUNT(this) FROM %s this";
+    private final static String SELECT_COUNT_ALL_WHERE = "SELECT COUNT(this) FROM %s this WHERE %s";
+    private final static String SELECT_1_AS_ONE = "SELECT 1 AS one FROM %s this WHERE %s";
 
     private final Class<T> entityClass;
 
@@ -23,7 +23,7 @@ public abstract class ApplicationService<T> {
         this.entityClass = entityClass;
     }
 
-    public abstract EntityManager getEntityManager();
+    protected abstract EntityManager getEntityManager();
 
     public abstract T find(String id);
 
@@ -51,58 +51,77 @@ public abstract class ApplicationService<T> {
         return getEntityManager().find(entityClass, id);
     }
 
+    public T findBy(String condition, Object... params) {
+        return parametize(createQuery(format(SELECT_ALL, entityName())), params).setMaxResults(1).getResultStream().findFirst().orElse(null);
+    }
+
     public List<T> all() {
-        return getEntityManager().createQuery(format(SELECT_STAR, entityClass.getSimpleName())).getResultList();
+        return createQuery(format(SELECT_ALL, entityName())).getResultList();
     }
 
-    public List<T> all(String order) {
-        return getEntityManager().createQuery(format(SELECT_STAR_ORDER, entityClass.getSimpleName(), order)).getResultList();
+    public List<T> order(String order) {
+        return createQuery(format(SELECT_ALL_ORDER, entityName(), order)).getResultList();
     }
 
-    public List<T> all(int limit, int offset) {
-        return getEntityManager().createQuery(format(SELECT_STAR, entityClass.getSimpleName())).setMaxResults(limit).setFirstResult(offset).getResultList();
+    public List<T> order(String order, int limit) {
+        return createQuery(format(SELECT_ALL_ORDER, entityName(), order)).setMaxResults(limit).getResultList();
     }
 
-    public List<T> all(String order, int limit, int offset) {
-        return getEntityManager().createQuery(format(SELECT_STAR_ORDER, entityClass.getSimpleName(), order)).setMaxResults(limit).setFirstResult(offset).getResultList();
+    public List<T> order(String order, int limit, int offset) {
+        return createQuery(format(SELECT_ALL_ORDER, entityName(), order)).setMaxResults(limit).setFirstResult(offset).getResultList();
     }
 
-    public List<T> where(String conditions, Object... params) {
-        Query query = getEntityManager().createQuery(format(SELECT_STAR_WHERE, entityClass.getSimpleName(), conditions));
-        setPositionalParameters(query, params);
-        return query.getResultList();
+    public List<T> where(String condition, Object... params) {
+        return parametize(createQuery(format(SELECT_ALL_WHERE, entityName(), condition)), params).getResultList();
     }
 
-    public List<T> where(String conditions, String order, Object... params) {
-        Query query = getEntityManager().createQuery(format(SELECT_STAR_WHERE_ORDER, entityClass.getSimpleName(), conditions, order));
-        setPositionalParameters(query, params);
-        return query.getResultList();
+    public List<T> whereLimit(String condition, int limit, Object... params) {
+        return parametize(createQuery(format(SELECT_ALL_WHERE, entityName(), condition)), params).setMaxResults(limit).getResultList();
     }
 
-    public List<T> where(String conditions, int limit, int offset, Object... params) {
-        Query query = getEntityManager().createQuery(format(SELECT_STAR_WHERE, entityClass.getSimpleName(), conditions)).setMaxResults(limit).setFirstResult(offset);
-        setPositionalParameters(query, params);
-        return query.getResultList();
+    public List<T> whereInRange(String condition, int limit, int offset, Object... params) {
+        return parametize(createQuery(format(SELECT_ALL_WHERE, entityName(), condition)), params).setMaxResults(limit).setFirstResult(offset).getResultList();
     }
 
-    public List<T> where(String conditions, String order, int limit, int offset, Object... params) {
-        Query query = getEntityManager().createQuery(format(SELECT_STAR_WHERE_ORDER, entityClass.getSimpleName(), conditions, order)).setMaxResults(limit).setFirstResult(offset);
-        setPositionalParameters(query, params);
-        return query.getResultList();
+    public List<T> whereOrder(String condition, String order, Object... params) {
+        return parametize(createQuery(format(SELECT_ALL_WHERE_ORDER, entityName(), condition, order)), params).getResultList();
+    }
+
+    public List<T> whereOrderLimit(String condition, String order, int limit, Object... params) {
+        return parametize(createQuery(format(SELECT_ALL_WHERE_ORDER, entityName(), condition, order)), params).setMaxResults(limit).getResultList();
+    }
+
+    public List<T> whereOrderInRange(String condition, String order, int limit, int offset, Object... params) {
+        return parametize(createQuery(format(SELECT_ALL_WHERE_ORDER, entityName(), condition, order)), params).setMaxResults(limit).setFirstResult(offset).getResultList();
     }
 
     public Long count() {
-        return getEntityManager().createQuery(format(SELECT_COUNT_STAR, entityClass.getSimpleName()), Long.class).getSingleResult();
+        return createQuery(format(SELECT_COUNT_ALL, entityName()), Long.class).getSingleResult();
     }
 
-    public Long count(String conditions, Object... params) {
-        TypedQuery<Long> query = getEntityManager().createQuery(format(SELECT_COUNT_STAR_WHERE, entityClass.getSimpleName(), conditions), Long.class);
-        setPositionalParameters(query, params);
-        return query.getSingleResult();
+    public Long count(String condition, Object... params) {
+        return parametize(createQuery(format(SELECT_COUNT_ALL_WHERE, entityName(), condition), Long.class), params).getSingleResult();
     }
 
-    private void setPositionalParameters(Query query, Object[] positionalParameters) {
-        range(0, positionalParameters.length).forEach(i -> query.setParameter(i + 1, positionalParameters[i]));
+    public boolean exists(String condition, Object... params) {
+        return parametize(createQuery(format(SELECT_1_AS_ONE, entityName(), condition)), params).setMaxResults(1).getResultStream().findFirst().isPresent();
+    }
+
+    protected TypedQuery<T> createQuery(String qlString) {
+        return getEntityManager().createQuery(qlString, entityClass);
+    }
+
+    protected <T> TypedQuery<T> createQuery(String qlString, Class<T> resultClass) {
+        return getEntityManager().createQuery(qlString, resultClass);
+    }
+
+    private <T> TypedQuery<T> parametize(TypedQuery<T> query, Object[] parameters) {
+        range(0, parameters.length).forEach(i -> query.setParameter(i + 1, parameters[i]));
+        return query;
+    }
+
+    private String entityName() {
+        return entityClass.getSimpleName();
     }
 
 }
