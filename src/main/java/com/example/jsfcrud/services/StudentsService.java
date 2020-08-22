e05cabe36f77e3edae9ddd3db94e989b0825eb7b
Lázaro Nixon <lazaronixon@hotmail.com>
2020-08-22 17:13:42 -0300

    update

--- src/main/java/com/example/jsfcrud/services/StudentsService.java
+++ src/main/java/com/example/jsfcrud/services/StudentsService.java
@@ -2,19 +2,9 @@ package com.example.jsfcrud.services;
 
 import com.example.jsfcrud.models.Student;
 import javax.enterprise.context.ApplicationScoped;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 @ApplicationScoped
 public class StudentsService extends ApplicationService<Student> {
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    @Override
-    public EntityManager getEntityManager() {
-        return entityManager;
-    }
 
     public StudentsService() {
         super(Student.class);
