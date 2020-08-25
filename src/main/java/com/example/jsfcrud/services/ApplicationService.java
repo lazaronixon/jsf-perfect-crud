263e47669e98880cfded58f61266fbd94610becd
Lázaro Nixon <lazaronixon@hotmail.com>
2020-08-25 17:23:49 -0300

    update

--- src/main/java/com/example/jsfcrud/services/ApplicationService.java
+++ src/main/java/com/example/jsfcrud/services/ApplicationService.java
@@ -2,7 +2,7 @@ package com.example.jsfcrud.services;
 
 import com.activepersistence.service.Base;
 
-public abstract class ApplicationService<T> extends Base<T> {
+public abstract class ApplicationService<T, ID> extends Base<T, ID> {
 
     public ApplicationService(Class<T> entityClass) {
         super(entityClass);
--- src/main/java/com/example/jsfcrud/services/StudentsService.java
+++ src/main/java/com/example/jsfcrud/services/StudentsService.java
@@ -4,7 +4,7 @@ import com.example.jsfcrud.models.Student;
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class StudentsService extends ApplicationService<Student> {
+public class StudentsService extends ApplicationService<Student, Long> {
 
     public StudentsService() {
         super(Student.class);
